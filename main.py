397a723cfb9c8052a6acb6b8532f4f60754a6596
Anastasia Mokhova <mokhova813@gmail.com>
2019-11-14 23:47:15 +0300

    Change 2

--- main.py
+++ main.py
@@ -14,13 +14,25 @@ def my_form():
 def my_form_post():
     api_alc = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Alcoholic"
     api_nonalc = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Non_Alcoholic"
-    api_byName = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita"
+    api_byName = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s="
 
-    json_obj = urllib.request.urlopen(api_alc)
-    data = json.load(json_obj)
+    json_obj_alc = urllib.request.urlopen(api_alc)
+    data_alc = json.load(json_obj_alc)
+
+    json_obj_nonalc = urllib.request.urlopen(api_nonalc)
+    data_nonalc = json.load(json_obj_nonalc)
+
+    json_obj_byName = urllib.request.urlopen(api_byName+(request.form.get("byName")).replace(' ', '%20'))
+    data_byName = json.load(json_obj_byName)
+
     if request.form.get("alc"):
-        return render_template('search.html', title='Home', data=data["drinks"])
-    return("error!")
+        return render_template('search.html', title='Home', data_alc=data_alc["drinks"])
+    if request.form.get("nonalc"):
+        return render_template('search.html', title='Home', data_nonalc=data_nonalc["drinks"])
+    if request.form.get("byName"):
+        return render_template('search.html', title='Home', data_byName=data_byName["drinks"])
+    return ("error!")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
