32955d0ae39a13351ac208ae33dbcb2c5632817b
Anastasia Mokhova <mokhova813@gmail.com>
2019-11-24 22:07:04 +0300

    Add README

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request
 import urllib.request
-import json, requests
+import json
 
 app = Flask(__name__)
 
@@ -12,36 +12,60 @@ def my_form():
 
 @app.route('/search', methods=['POST'])
 def my_form_post():
+    str_nonalc = ""
+    str_alc = ""
     api_alc = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Alcoholic"
     api_nonalc = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Non_Alcoholic"
     api_byName = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s="
+    api_byIngredient = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i="
 
 
-    if request.form.get("alc"):
-        json_obj_alc = urllib.request.urlopen(api_alc)
-        data_alc = json.load(json_obj_alc)
-        return render_template('search.html', title='Home', data_alc=data_alc["drinks"])
+    if request.form.get("alc") or request.form.get("nonalc"):
+        if request.form.get("alc"):
+            json_obj_alc = urllib.request.urlopen(api_alc)
+            data_alc = json.load(json_obj_alc)
+            str_alc = render_template('list.html', data=data_alc["drinks"])
 
-    if request.form.get("nonalc"):
-        json_obj_nonalc = urllib.request.urlopen(api_nonalc)
-        data_nonalc = json.load(json_obj_nonalc)
-        return render_template('search.html', title='Home', data_nonalc=data_nonalc["drinks"])
+        if request.form.get("nonalc"):
+            json_obj_nonalc = urllib.request.urlopen(api_nonalc)
+            data_nonalc = json.load(json_obj_nonalc)
+            str_nonalc = render_template('list.html', data=data_nonalc["drinks"])
 
-    if request.form.get("byName"):
+        return render_template('search.html', s1=str_alc, s2=str_nonalc)
+
+
+    elif request.form.get("byName"):
         json_obj_byName = urllib.request.urlopen(api_byName + (request.form.get("byName")).replace(' ', '%20'))
         data_byName = json.load(json_obj_byName)
-        return render_template('search.html', title='Home', data_byName=data_byName["drinks"])
-    return ("error!")
+        str_byName = render_template('list.html',  data=data_byName["drinks"])
+        return render_template('searchByName.html', s1=str_byName)
+
+    elif request.form.get("byIngredient"):
+        json_obj_byIngredient = urllib.request.urlopen(api_byIngredient + request.form.get("byIngredient").replace(' ', '%20'))
+        data_byIngredient = json.load(json_obj_byIngredient)
+        str_byIngredient = render_template('list.html',  data=data_byIngredient["drinks"])
+        return render_template('searchByIngredient.html', s1=str_byIngredient)
+
+    else:
+        return "ERROR!"
+
 
 @app.route('/find', methods=['GET'])
 def show_recipe():
     drink_id = request.args.get("id")
-    api_byId = "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i="
 
-    json_obj_byId = urllib.request.urlopen(api_byId+drink_id)
-    api_byId = json.load(json_obj_byId)
-    return api_byId
+    if drink_id is not None:
+        api_byId = "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i="
+        json_obj_byId = urllib.request.urlopen(api_byId + drink_id)
+        data_byId = json.load(json_obj_byId)
+        return render_template('foundCocktail.html', data=data_byId['drinks'][0])
 
+
+    else:
+        api_Random = "https://www.thecocktaildb.com/api/json/v1/1/random.php"
+        json_Random = urllib.request.urlopen(api_Random)
+        data_Random = json.load(json_Random)
+        return render_template('foundCocktail.html', data=data_Random['drinks'][0])
 
 if __name__ == "__main__":
     app.run(debug=True)
