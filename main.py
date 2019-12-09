77592782229dea675262b08b6e0af5a3aa90493a
Anastasia Mokhova <mokhova813@gmail.com>
2019-12-09 22:38:03 +0300

    Added authorization

--- main.py
+++ main.py
@@ -1,14 +1,65 @@
-from flask import Flask, render_template, request
-import urllib.request
+import bcrypt
 import json
+import urllib.request
+
+from flask import Flask, render_template, request, session, redirect
+from pymongo import MongoClient
 
 app = Flask(__name__)
+
+client = MongoClient()
+
+client = MongoClient("mongodb+srv://mokhova813:12345@cluster0-yl1dx.mongodb.net/test?retryWrites=true&w=majority")
+
+db = client.registration_database
+collection = db.registration_collection
 
 
 @app.route('/')
 def my_form():
-    return render_template('index.html')
+    # if 'username' in session:
+    #     return render_template('index.html', data='You are logged in as ' + session['username'])
+    # else:
+    return render_template('index.html', reg=1)
 
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        users = collection
+        login_user = users.find_one({'name': request.form['username']})
+
+        if login_user:
+            if bcrypt.hashpw(unicode(request.form['pass']), login_user['password'].encode('utf-8')) == \
+                    login_user['password'].encode('utf-8'):
+                session['username'] = request.form['username']
+                return redirect('/')
+            return 'Invalid username/password combination'
+    return render_template('login.html')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        users = collection
+        existing_user = users.find_one({'name': request.form['username']})
+
+        if existing_user is None:
+            hashpass = bcrypt.hashpw(request.form['pass'].encode('utf-8'), bcrypt.gensalt())
+            users.insert({'name': request.form['username'], 'password': hashpass})
+            session['username'] = request.form['username']
+            # return redirect(url_for('index'))
+            return redirect('/')
+
+        return 'That username already exists!'
+
+    return render_template('registration.html')
+
+
+# @app.route('/logout')
+# def logout():
+#
+#     return redirect(url_for('login'))
 
 @app.route('/search', methods=['POST'])
 def my_form_post():
@@ -18,7 +69,6 @@ def my_form_post():
     api_nonalc = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Non_Alcoholic"
     api_byName = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s="
     api_byIngredient = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i="
-
 
     if request.form.get("alc") or request.form.get("nonalc"):
         if request.form.get("alc"):
@@ -37,13 +87,14 @@ def my_form_post():
     elif request.form.get("byName"):
         json_obj_byName = urllib.request.urlopen(api_byName + (request.form.get("byName")).replace(' ', '%20'))
         data_byName = json.load(json_obj_byName)
-        str_byName = render_template('list.html',  data=data_byName["drinks"])
+        str_byName = render_template('list.html', data=data_byName["drinks"])
         return render_template('searchByName.html', s1=str_byName)
 
     elif request.form.get("byIngredient"):
-        json_obj_byIngredient = urllib.request.urlopen(api_byIngredient + request.form.get("byIngredient").replace(' ', '%20'))
+        json_obj_byIngredient = urllib.request.urlopen(
+            api_byIngredient + request.form.get("byIngredient").replace(' ', '%20'))
         data_byIngredient = json.load(json_obj_byIngredient)
-        str_byIngredient = render_template('list.html',  data=data_byIngredient["drinks"])
+        str_byIngredient = render_template('list.html', data=data_byIngredient["drinks"])
         return render_template('searchByIngredient.html', s1=str_byIngredient)
 
     else:
@@ -67,11 +118,7 @@ def show_recipe():
         data_Random = json.load(json_Random)
         return render_template('foundCocktail.html', data=data_Random['drinks'][0])
 
-if __name__ == "__main__":
-    app.run(debug=True)
 
-# client = pymongo.MongoClient("mongodb://mokhova813:12345@cluster0-yl1dx.mongodb.net/test?retryWrites=true&w=majority")
-# db = client.test
-# collection = db['test']
-#
-# collection.insert_one({'name' : "aa"})
+if __name__ == "__main__":
+    app.secret_key = 'mysecret'
+    app.run(debug=True)
