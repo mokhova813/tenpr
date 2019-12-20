2576dffc0506ab6ab1ae29780755900d27e9513b
fefilova <allblue312@gmail.com>
2019-12-20 17:43:06 +0300

    Your message

--- pro1/src/pages/Login.jsx
+++ pro1/src/pages/Login.jsx
@@ -28,35 +28,37 @@ export default class Login extends React.Component {
             </Link>
           </Form>
         </Col>
-      </div>
-      <div class="parent" >
-        <div class="block">
-          <Form>
-            <div class="contact_form h2">
-              <Form.Label>
-                <h2>ВХОД</h2>
-              </Form.Label>
-            </div>
-            <div class="text-field contact_form input">
-              <Form.Group controlId="formBasicEmail">
-                <Form.Control type="email" placeholder="Enter email" />
-              </Form.Group>
-            </div>
-            <div class="contact_form input">
-              <Form.Group controlId="formBasicPassword">
-                <Form.Control type="password" placeholder="Password" />
-              </Form.Group>
-            </div>
-            <ul>
-          <button class="submit" type="submit">
-            <Link to="/Home">
-             Войти
+
+        <div class="parent" >
+          <div class="block">
+            <Form>
+              <div class="contact_form h2">
+                <Form.Label>
+                  <h2>ВХОД</h2>
+                </Form.Label>
+              </div>
+              <div class="text-field contact_form input">
+                <Form.Group controlId="formBasicEmail">
+                  <Form.Control type="email" placeholder="Enter email" />
+                </Form.Group>
+              </div>
+              <div class="contact_form input">
+                <Form.Group controlId="formBasicPassword">
+                  <Form.Control type="password" placeholder="Password" />
+                </Form.Group>
+              </div>
+              <ul>
+                <button class="submit" type="submit">
+                  <Link to="/Home">
+                    Войти
             </Link>
-            </button>
-            </ul>
-          </Form>
+                </button>
+              </ul>
+            </Form>
+          </div>
         </div>
       </div>
+
     )
   }
 }
